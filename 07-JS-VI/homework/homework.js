59f06233e24415ae911a5b7fe44a9cb749a34942
hibarra93 <hector.aim12@gmail.com>
2022-09-07 20:06:43 -0700

    Actualizacion de tarea 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,10 +45,12 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var newarray = [];
-  for(var i = 0; i < array.length; i++){
+  var newarray = array.map(cb);
+  
+  
+  /*for(var i = 0; i < array.length; i++){
     newarray[i] = cb(array[i]);
-  }
+  }*/
   return newarray;
 }
 
